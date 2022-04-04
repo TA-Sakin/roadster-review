dadacd6e99d8d94c8f26b431de32b9486ef9bba6
TA-Sakin <tasakin98@gmail.com>
2022-04-05 02:08:04 +0600

    readme added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <h3>404, not found</h3>
+      <h3 className="text-center mt-5">404, page not found...</h3>
     </div>
   );
 };
